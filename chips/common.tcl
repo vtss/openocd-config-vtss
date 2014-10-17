b858d81075e45bf948640729668ebc121673a3c3
Lars Povlsen <lpovlsen@vitesse.com>
2014-10-17 16:06:56 +0200

    Add 'cpu' command for mips32 exception debugging support

--- chips/common.tcl
+++ chips/common.tcl
@@ -30,3 +30,28 @@ proc show_reg {NAME flds} {
         error [format "%s (%s)" $msg $NAME ]
     }
 }
+
+proc mips_reg {reg sel} {
+    set val [capture "mips32 cp0 $reg $sel"]
+    if [regexp {: (\w{8})} $val -> hexval] {
+        return "0x$hexval"
+    } else {
+        return 0
+    }
+}
+
+proc cpu {} {
+    set cause [mips_reg 13 0]
+    set cf [decode_fields $cause { {bd 31 31} {ti 30 30} {ce 29 28} {dc 27 27} {pci 26 26} {iv 23 23} {wp 22 22} {ip72_ripl 15 10} {ip10 9 8} {exccode 6 2} } ]
+    set status [mips_reg 12 0]
+    set sf [decode_fields $status { {cu 31 28} {rp 27 27} {fr 26 26} {re 25 25} {mx 24 24} {bev 22 22} {ts 21 21} {sr 20 20} {nmi 19 19} {cee 17 17} {ip72_ripl 15 10} {im10 9 8} {ksu 4 3} {erl 2 2} {exl 1 1} {ie 0 0} } ]
+    set badvaddr [mips_reg 8 0]
+    set epc [mips_reg 14 0]
+    set errorepc [mips_reg 30 0]
+    echo [format "%-10s: 0x%08x %s" ".cause"    $cause    $cf]
+    echo [format "%-10s: 0x%08x %s" ".status"   $status   $sf]
+    echo [format "%-10s: 0x%08x"    ".badvaddr" $badvaddr $sf]
+    echo [format "%-10s: 0x%08x"    ".epc"      $epc         ]
+    echo [format "%-10s: 0x%08x"    ".errorepc" $errorepc    ]
+    echo "exccode: 0=int, 1=tlbmod, 2=tlbload, 3=tlbstore, 4=addrload, 5=addrstore, 9=bkpt, a=res_instr, ..."
+}
